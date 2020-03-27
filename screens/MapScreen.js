3ae00119f9dbef1738001d9d99f4f1fa1f512738
Tiago <tiagobit22@hotmail.com>
2020-03-27 20:49:58 -0300

    Update MapScreen.js
    MapScreen Att

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,44 +1,79 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import NavigatorTab from '../components/Tabs/NavigatorTab';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
 
-export default function MapScreen({ navigation }) {
 
-  const region = navigator.geolocation.getCurrentPosition(
-    ({ coords: {latitude, longitude} }) => {
-      return {
-        latitude,
-        longitude,
-        latitudeDelta: 0.0143,
-        longitudeDelta: 0.0134
-      }
+export default class MapScreen extends React.Component {
+
+  state = {
+    latitude: null,
+    longitude: null,
+  }
+
+  //Função para pegar a posição atual da pessoa e definir no State.
+  async componentDidMount() {
+      ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }),
+    navigator.geolocation.getCurrentPosition(
+      (error) => console.log('Error:', error)
+    )
+
+  }
+  render() {
+    //Pega as informaçoes do State para utilizar no initialRegion.
+    const { latitude, longitude } = this.state
+
+    if (latitude) {
+      return (
+        <View style={styles.mapView}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude,
+              longitude,
+              latitudeDelta: 0,
+              longitudeDelta: 0.001,
+            }}
+            showsUserLocation
+            rotateEnabled={false}
+          />
+        </View>
+      );
     }
-  )
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('../assets/images/van.png')}
+          style={styles.image}
+        />
+      </View>
+    )
 
-  return (
-    <View style={styles.container}>
-      <MapView 
-        style={styles.mapStyle}
-        region={region}
-        showsUserLocation
-        loadingEnabled  
-      />
-
-      <NavigatorTab leftBtn={() => navigation.navigate('Home')}/>
-    </View>
-  );
+  }
 }
 
 const styles = StyleSheet.create({
+
   container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: '90%',
+    flex: 1
+  },
+  mapView: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: '90%',
-  },
-});
+  }
+})
